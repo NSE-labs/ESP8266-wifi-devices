43d597a47b30bc88a31049fc56bfbf7deee5247d
Kevin <fixcars@frontier.com>
2017-06-02 10:32:01 -0500

    Initial version of web server that works
    Works for both GET and POST

--- Web server/web_server.py
+++ Web server/web_server.py
@@ -11,29 +11,36 @@ micropython.alloc_emergency_exception_buf(100)
 
 
 def ok(socket, query):
-    socket.write("HTTP/1.1 OK\r\n\r\n")
-    socket.write("<!DOCTYPE html><html><title>"+TITLE+"</title><body>")
-    socket.write("<form method='POST' action='/action?'>")
-    socket.write("  Access point: <input type='text' name='ap'>")
-    socket.write("<input type='submit' value='Submit'>")
-    socket.write("</form></body></html>")
-                 
+    socket.write('HTTP/1.1 200 OK\r\n\r\n')
+    socket.write('<!DOCTYPE html><html><title>'+TITLE+'</title><body>')
+    socket.write('<form method="POST" action="/action?">')
+    socket.write('<input type="text" name="ap">')
+    socket.write('<input type="submit" value="Submit">')
+    socket.write('</form></body></html>')
+
 
 def err(socket, code, message):
+    print('Error:', code, message)
     socket.write("HTTP/1.1 "+code+" "+message+"\r\n\r\n")
     socket.write("<h1>"+message+"</h1>")
 
-                 
+
 def handle(socket):
-    (method, url, version) = socket.readline().split(b" ")
-    print(method, url, version)
-    if b"?" in url:
-        (path, query) = url.split(b"?", 2)
+    line = socket.readline()
+    print(line)
+    if(line):
+        (method, url, version) = line.split(b" ")
+        print(method, url, version)
+        if b"?" in url:
+            (path, query) = url.split(b"?", 2)
+        else:
+            (path, query) = (url, b"")
+        print(path, query)
     else:
-        (path, query) = (url, b"")
-    print(path, query)
+        return
     while True:
         header = socket.readline()
+        print(header)
         if header == b"":
             return
         if header == b"\r\n":
@@ -43,14 +50,19 @@ def handle(socket):
         err(socket, "505", "Version Not Supported")
     elif method == b"GET":
         if path == b"/":
+            print('Calling ok for GET')
             ok(socket, query)
+            print('returned from ok for GET')
         else:
             err(socket, "404", "Not Found")
     elif method == b"POST":
         if path == b"/action":
-            print('Calling ok')
+            print('Reading body of message')
+            body = socket.recv(1024)
+            print(body)
+            print('Calling ok for POST')
             ok(socket, query)
-            print('Returned from ok')
+            print('Returned from ok for POST')
         else:
             err(socket, "404", "Not Found")
     else:
@@ -59,12 +71,14 @@ def handle(socket):
 server = usocket.socket()
 server.bind(('0.0.0.0', 8080))
 server.listen(1)
+print('Listening on port 8080')
 while True:
-    try:
-        (socket, sockaddr) = server.accept()
-        print("Received request")
-        handle(socket)
-    except:
-        socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n")
-        socket.write("<h1>Internal Server Error</h1>")
+#    try:
+    (socket, sockaddr) = server.accept()
+    print("Received request from", sockaddr)
+    handle(socket)
+#    except:
+#        print('Exception!')
+#        socket.write("HTTP/1.1 500 Internal Server Error\r\n\r\n")
+#        socket.write("<h1>Internal Server Error</h1>")
     socket.close()
