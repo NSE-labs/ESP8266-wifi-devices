a363eeb5517ddb395f5d817baccebf002d151c88
Kevin <fixcars@frontier.com>
2017-06-16 11:52:49 -0500

    Implemented my_config

--- MyLib/my_config.py
+++ MyLib/my_config.py
@@ -1,3 +1,28 @@
 """
 Save and get configuration data from a file
 """
+import ujson
+
+FILENAME = 'config'
+
+def save(obj):
+    """ Saves the passed in object to FILENAME """
+    file = open(FILENAME, 'w')
+    file.write(ujson.dumps(obj))
+    file.close()
+
+
+def get():
+    """
+    Gets an object from FILENAME
+    returns the object or None if FILENAME doesn't exist or
+    if the file doesn't contain a valid object
+    """
+    try:
+        file = open(FILENAME)
+        str = file.read()
+        file.close()
+        obj = ujson.loads(str)
+    except (OSError, ValueError):
+        return None
+    return obj
